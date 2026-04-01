d85d8ddc96832af382f249fb05a66479e25c1e31
Pavel Vazharov <pavel@x3me.net>
2026-04-01 15:29:00 +0300

    Add generic formatter for types with operator<<(std::ostream&, ...)

--- app/formatters.hpp
+++ app/formatters.hpp
@@ -2,20 +2,24 @@
 
 namespace lcd
 {
-
+// The types which have both std::formatter specialization and operator<<
+// needs to be listed to avoid compilation errors for ambitious calls.
 template <typename T>
-concept streamable = requires(std::ostream& os, const T& v) {
+concept stream_formattable = requires(std::ostream& os, const T& v) {
+    requires !std::is_same_v<T, char>;
+    requires !std::is_same_v<T, const char*>;
+    requires !std::is_same_v<T, const void*>;
     { os << v } -> std::convertible_to<std::ostream&>;
 };
 
 } // namespace lcd
 ////////////////////////////////////////////////////////////////////////////////
-template <lcd::streamable T>
+template <lcd::stream_formattable T>
 struct std::formatter<T> : std::formatter<std::string_view>
 {
     auto format(const T& v, auto& ctx) const noexcept
     {
-        std::istringstream ss;
+        std::ostringstream ss;
         ss << v;
         return std::formatter<std::string_view>::format(ss.view(), ctx);
     }
